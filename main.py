6b380309a3a6d0cd61a16df50081b01c4b0f9934
Abubakar Bunza Mubarak <mubarakabubakar227@gmail.com>
2020-12-08 23:07:47 +0000

    added tower_builder()

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-odd = []
-n = 6
-i = 0
-while len(odd) < n:
-  if i%2 != 0:
-    odd.append(i)
-  i += 1
-for i in range(len(odd)):
-  space = odd[-1] - odd[i]
-  print(' '*i)
-print(odd)
+def tower_builder(n_floors):
+    odd = []
+    tower = []
+    i = 0
+    while len(odd) < n_floors:
+        if i%2 != 0:
+            odd.append(i)
+        i += 1
+    for i in range(len(odd)):
+        space = ' '*((odd[-1] - odd[i])//2)
+        star = '*'*odd[i]
+        tower.append((f'{space}{star}{space}'))
+    
+    return tower
+
+print(tower_builder(2))
